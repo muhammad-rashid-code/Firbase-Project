7e64d154d697c0955917c86e99e5151469940b7a
muhammad rashid <m.rashid212823@gmail.com>
2024-09-29 19:39:44 +0500

    thori c good practice

--- firebase-learning/src/app/home/page.tsx
+++ firebase-learning/src/app/home/page.tsx
@@ -3,12 +3,56 @@
 import { AuthContextExport } from "@/context/auth-context";
 import ButtonComponents from "../components/page";
 import { serviceSignOut } from "@/firebase/2firebase-auth";
-import { useState } from "react";
-import { serviceSaveToDo } from "@/firebase/3firebase-cloudfirestore";
+import { useEffect, useState } from "react";
+import { auth, db, serviceSaveToDo } from "@/firebase/3firebase-cloudfirestore";
+import {
+  collection,
+  DocumentData,
+  onSnapshot,
+  query,
+  Unsubscribe,
+  where,
+} from "firebase/firestore";
+import { onAuthStateChanged } from "firebase/auth";
 
 export default function Home() {
   const { user } = AuthContextExport()!;
   const [todoHuS, setTodoHuS] = useState<string>("");
+  const [allTodosHuS, setAllTodosHuS] = useState<DocumentData[]>([]);
+  useEffect(() => {
+    let detachOnAuthStateListner = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        fetchTodosRealtime();
+      }
+    });
+    return () => {
+      if (readTodosRealtime) {
+        readTodosRealtime();
+        detachOnAuthStateListner();
+      }
+    };
+  }, []);
+
+  let readTodosRealtime: Unsubscribe;
+
+  const fetchTodosRealtime = () => {
+    let collectionRef = collection(db, "todos");
+    let c = auth.currentUser?.uid;
+    let condition = where("uid", "==", c);
+    let q = query(collectionRef, condition);
+    let allTodosClone = [...allTodosHuS];
+
+    readTodosRealtime = onSnapshot(q, (querySnapShot) => {
+      querySnapShot.docChanges().forEach((change) => {
+        if (change.type === "added") {
+          let todo = change.doc.data();
+          todo.id = change.doc.id;
+          allTodosClone.push(todo);
+          setAllTodosHuS([...allTodosClone]);
+        }
+      });
+    });
+  };
   return (
     <>
       <h1>Welcome Home</h1>
@@ -37,6 +81,13 @@ export default function Home() {
           serviceSignOut();
         }}
       />
+      {allTodosHuS.length > 0 ? (
+        allTodosHuS.map(({ todo }) => <h1>{todo}</h1>)
+      ) : (
+        <>
+          <p>there is no todo</p>
+        </>
+      )}
     </>
   );
 }
